a43151528ad29ac3ca8a4aa02ad3f56a8eeda787
beta0221 <beta0221@gmail.com>
2024-06-19 16:45:06 +0800

    註冊 信箱驗證 修正

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -15,13 +15,7 @@ class VerifyEmailController extends Controller
      */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
-        if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(RouteServiceProvider::HOME . '?verified=1');
-        }
-
-        if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
-        }
+        $request->fulfill();
 
         return redirect()->intended(RouteServiceProvider::HOME . '?verified=1');
     }
